1a90e148f5f52c8b1d7e04adfd70f64830ecad72
Fábio Amaral <fabio.pamaral@outlook.com>
2025-01-27 11:35:18 -0300

    creation of GroupsController and implementation of routes

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { LeadsController } from "./controllers/LeadsController";
 import { PrismaLeadsRepository } from "./repositories/prisma/PrismaLeadsRepository";
+import { GroupsController } from "./controllers/GroupsController";
 
 export const router = Router();
 
 const leadsRepository = new PrismaLeadsRepository();
+const groupsRepository = new GroupsController();
 
 const leadsController = new LeadsController(leadsRepository);
 
@@ -13,6 +15,12 @@ router.get("/leads/:id", leadsController.show);
 router.post("/leads", leadsController.create);
 router.put("/leads/:id", leadsController.update);
 router.delete("/leads/:id", leadsController.delete);
+
+router.get("/groups", groupsRepository.index);
+router.post("/groups", groupsRepository.create);
+router.get("/groups/:id", groupsRepository.show);
+router.put("/groups/:id", groupsRepository.update);
+router.delete("/groups/:id", groupsRepository.delete);
 
 router.get("/teste", (req, res) => {
   res.json({ message: "TUDO OK" });
