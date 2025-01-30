abe151fec44dc088dc951782570c5050c9a9b29d
Fábio Amaral <fabio.pamaral@outlook.com>
2025-01-30 16:43:41 -0300

    fix GroupsController instantiation

--- src/container.ts
+++ src/container.ts
@@ -6,6 +6,7 @@ import { LeadsController } from "./controllers/LeadsController";
 import { PrismaCampaignsRepository } from "./repositories/prisma/PrismaCampaignsRepository";
 import { PrismaGroupsRepository } from "./repositories/prisma/PrismaGroupsRepository";
 import { PrismaLeadsRepository } from "./repositories/prisma/PrismaLeadsRepository";
+import { GroupsService } from "./services/GroupsService";
 import { LeadsService } from "./services/LeadsService";
 
 export const leadsRepository = new PrismaLeadsRepository();
@@ -13,6 +14,7 @@ export const groupsRepository = new PrismaGroupsRepository();
 export const campaignsRepository = new PrismaCampaignsRepository();
 
 export const leadsService = new LeadsService(leadsRepository);
+export const groupsService = new GroupsService(groupsRepository);
 
 export const groupLeadsController = new GroupLeadsController(
   leadsRepository,
@@ -24,5 +26,5 @@ export const campaignLeadsController = new CampaignLeadsController(
 );
 
 export const leadsController = new LeadsController(leadsService);
-export const groupsController = new GroupsController(groupsRepository);
+export const groupsController = new GroupsController(groupsService);
 export const campaignsController = new CampaignsController(campaignsRepository);
